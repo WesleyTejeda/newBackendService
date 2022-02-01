ad206a1e9301b7830a078f86cc47074744af57cf
Dillon C Baker <73920614+dcbaker1992@users.noreply.github.com>
2022-02-01 11:03:50 -0600

    backend without passport

---
+++ routes/index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+var Schema = mongoose.Schema;
+const md5 = require('md5');
+
+var crypto = require('crypto');
+
+var userSchema = new Schema ({
+  username: String,
+  password: String,
+  sessionID: String
+});
+
+var userModel = mongoose.model('userModel', userSchema);
+
+db.on('error', console.error.bind(console, 'MongoDB error'));
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.send('');
+});
+
+router.post('/signup', (req, res, next) => {
+  userModel.create({username: req.body.username, 
+    password: md5(req.body.password), 
+    sessionID: crypto.randomBytes(32).toString('base64')}, (err, msg) => {
+    res.send(err ? err : msg);
+  });
+});
+
+router.post('/login', (req, res, next) => {
+  userModel.findOne({username: req.body.username}, '_id username password sessionID', (err, acc) => {
+    if (!err && acc) {
+      if (md5(req.body.password) == acc.password) return res.send({
+        sessionID: acc.sessionID,
+        customerID: acc._id
+      });
+    }
+
+    return res.send({err: 'invalid credentials'});
+  });
+});
+
+router.post('/auth', (req, res, next) => {
+  userModel.findOne({sessionID: req.body.sessionID}, '_id username sessionID', (err, acc) => {
+    if (!err && acc) {
+      if (req.body.username == acc.username) return res.send({
+        customerID: acc._id
+      });
+    }
+
+    return res.send({err: 'invalid sessionID'});
+  })
+});
+
+module.exports = router;
